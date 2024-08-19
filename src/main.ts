cd73d343ec9f43395b5e054473bc44f965096e9a
AronCPX <cipeax@gmail.com>
2024-08-20 00:08:11 +0200

    rebound down to c

--- src/main.ts
+++ src/main.ts
@@ -22,10 +22,12 @@ let direction = new THREE.Vector3();
 
 function init() {
   scene = new THREE.Scene();
-  canvas = document.querySelector('#canvas');
-  const blocker = document.querySelector('#blocker') as HTMLElement;
-	const instructions = document.querySelector('#instructions') as HTMLElement;
-  let instructionTitle = document.querySelector('#instruction-title') as HTMLElement;
+  canvas = document.querySelector("#canvas");
+  const blocker = document.querySelector("#blocker") as HTMLElement;
+  const instructions = document.querySelector("#instructions") as HTMLElement;
+  let instructionTitle = document.querySelector(
+    "#instruction-title"
+  ) as HTMLElement;
 
   renderer = new THREE.WebGLRenderer({
     canvas: canvas,
@@ -44,13 +46,16 @@ function init() {
   const light = new THREE.AmbientLight(0x404040);
   scene.add(light);
 
-  const geometry = new THREE.PlaneGeometry( 125, 90);
-  const material = new THREE.MeshBasicMaterial( {color: 0x222222, side: THREE.DoubleSide} );
-  const plane = new THREE.Mesh( geometry, material );
-  plane.rotateX(Math.PI/2)
+  const geometry = new THREE.PlaneGeometry(125, 90);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x222222,
+    side: THREE.DoubleSide,
+  });
+  const plane = new THREE.Mesh(geometry, material);
+  plane.rotateX(Math.PI / 2);
 
   controls = new PointerLockControls(camera, renderer.domElement);
-  controls.unlock()
+  controls.unlock();
 
   clock = new THREE.Clock();
 
@@ -61,13 +66,19 @@ function init() {
       object.scale.set(0.01, 0.01, 0.01);
       console.log("Ready");
       scene.add(object);
-      scene.add( plane );
-      instructionTitle.innerText = "Click to begin.\n\n"
+      scene.add(plane);
+      instructionTitle.innerText = "Click to begin.\n\n";
     },
     (xhr) => {
-      console.log(parseFloat(((xhr.loaded / xhr.total) * 100).toString()).toFixed(2) + "% loaded");
-      let loadingText = "Loading...\n" + parseInt(((xhr.loaded / xhr.total) * 100).toString()) + "%"
-      if (loadingText !== instructionTitle.innerText){
+      console.log(
+        parseFloat(((xhr.loaded / xhr.total) * 100).toString()).toFixed(2) +
+          "% loaded"
+      );
+      let loadingText =
+        "Loading...\n" +
+        parseInt(((xhr.loaded / xhr.total) * 100).toString()) +
+        "%";
+      if (loadingText !== instructionTitle.innerText) {
         instructionTitle.innerText = loadingText;
       }
     },
@@ -76,120 +87,118 @@ function init() {
     }
   );
 
-  const onKeyDown = function ( event: { code: any; } ) {
-    switch ( event.code ) {
-      case 'KeyW':
+  const onKeyDown = function (event: { code: any }) {
+    switch (event.code) {
+      case "KeyW":
         moveForward = true;
         break;
-      case 'KeyA':
+      case "KeyA":
         moveLeft = true;
         break;
-      case 'KeyS':
+      case "KeyS":
         moveBackward = true;
         break;
-      case 'KeyD':
+      case "KeyD":
         moveRight = true;
         break;
-      case 'Space':
+      case "Space":
         moveUp = true;
         break;
-      case 'ControlLeft':
+      case "KeyC":
         moveDown = true;
         break;
-      case 'ShiftLeft':
+      case "ShiftLeft":
         shiftPressed = true;
         break;
     }
   };
 
-  const onKeyUp = function ( event: { code: any; } ) {
-    switch ( event.code ) {
-      case 'KeyW':
+  const onKeyUp = function (event: { code: any }) {
+    switch (event.code) {
+      case "KeyW":
         moveForward = false;
         break;
-      case 'KeyA':
+      case "KeyA":
         moveLeft = false;
         break;
-      case 'KeyS':
+      case "KeyS":
         moveBackward = false;
         break;
-      case 'KeyD':
+      case "KeyD":
         moveRight = false;
         break;
-      case 'Space':
+      case "Space":
         moveUp = false;
         break;
-      case 'ControlLeft':
+      case "KeyC":
         moveDown = false;
         break;
-      case 'ShiftLeft':
+      case "ShiftLeft":
         shiftPressed = false;
         break;
     }
   };
 
-  document.addEventListener( 'keydown', onKeyDown );
-  document.addEventListener( 'keyup', onKeyUp );
+  document.addEventListener("keydown", onKeyDown);
+  document.addEventListener("keyup", onKeyUp);
 
-  document.addEventListener("mousedown",()=>{
+  document.addEventListener("mousedown", () => {
     if (!unlockCooldown) {
       controls.lock();
-      
-      blocker.style.display = 'none';
-		  instructions.style.display = 'none';
+
+      blocker.style.display = "none";
+      instructions.style.display = "none";
     }
   });
-  controls.addEventListener('unlock', () => {
+  controls.addEventListener("unlock", () => {
     unlockCooldown = true;
-    instructionTitle.innerText = "Click to continue.\n\n"
-    blocker.style.display = 'block';
-		instructions.style.display = '';
+    instructionTitle.innerText = "Click to continue.\n\n";
+    blocker.style.display = "block";
+    instructions.style.display = "";
     setTimeout(() => {
       unlockCooldown = false;
-    }, 1200)
+    }, 1200);
   });
 
-window.onbeforeunload = function (e) {
-  e.preventDefault();
-  moveBackward = false;
-  moveDown = false;
-  moveUp = false;
-  moveLeft = false;
-  moveRight = false;
-  moveForward = false;
-};
+  window.onbeforeunload = function (e) {
+    e.preventDefault();
+    moveBackward = false;
+    moveDown = false;
+    moveUp = false;
+    moveLeft = false;
+    moveRight = false;
+    moveForward = false;
+  };
 
-window.addEventListener( 'resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-} );
-
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
 }
 
 function animate() {
   renderer.render(scene, camera);
   let delta = clock.getDelta();
 
-  if ( controls.isLocked === true ) {
-
+  if (controls.isLocked === true) {
     velocity.x -= velocity.x * 10.0 * delta;
     velocity.z -= velocity.z * 10.0 * delta;
     velocity.y -= velocity.y * 10.0 * delta;
 
-    direction.z = Number( moveForward ) - Number( moveBackward );
-    direction.x = Number( moveRight ) - Number( moveLeft );
-    direction.y = Number( moveUp ) - Number( moveDown );
+    direction.z = Number(moveForward) - Number(moveBackward);
+    direction.x = Number(moveRight) - Number(moveLeft);
+    direction.y = Number(moveUp) - Number(moveDown);
     direction.normalize();
 
     let speed = shiftPressed ? 400.0 : 150.0;
 
-    if ( moveForward || moveBackward ) velocity.z -= direction.z * speed * delta;
-    if ( moveLeft || moveRight ) velocity.x -= direction.x * speed * delta;
-    if ( moveUp || moveDown ) velocity.y -= direction.y * speed * delta;
+    if (moveForward || moveBackward) velocity.z -= direction.z * speed * delta;
+    if (moveLeft || moveRight) velocity.x -= direction.x * speed * delta;
+    if (moveUp || moveDown) velocity.y -= direction.y * speed * delta;
 
-    controls.moveRight( - velocity.x * delta );
-    controls.moveForward( - velocity.z * delta );
+    controls.moveRight(-velocity.x * delta);
+    controls.moveForward(-velocity.z * delta);
     camera.position.y -= velocity.y * delta;
   }
   requestAnimationFrame(animate);
